46b7cfd3e699917682aacebe0219418e141d30dd
Bruno Tomé <btomeduarte@gmail.com>
2024-12-16 10:50:18 -0300

    Feat: Fazendo conexao com o banco de dados junto ao cliente atraves da formataco de conexao do NODEjs

--- public/documento.js
+++ public/documento.js
@@ -1,24 +1,27 @@
-import { emitirTextoEditor, selecionarDocumento } from "./socket-front-documento.js";
-
-const parametros = new URLSearchParams(window.location.search);
-const nomeDocumento = parametros.get("nome");
-
-const textoEditor = document.getElementById("editor-texto");
-const tituloDoDocumento = document.getElementById("titulo-documento");
-
-tituloDoDocumento.textContent = nomeDocumento || "Documento sem titulo";
-
-selecionarDocumento(nomeDocumento);
-
-textoEditor.addEventListener("keyup", () => {
-    emitirTextoEditor({ 
-        texto: textoEditor.value, 
-        nomeDocumento 
+import {
+    emitirTextoEditor,
+    selecionarDocumento,
+  } from "./socket-front-documento.js";
+  
+  const parametros = new URLSearchParams(window.location.search);
+  const nomeDocumento = parametros.get("nome");
+  
+  const textoEditor = document.getElementById("editor-texto");
+  const tituloDocumento = document.getElementById("titulo-documento");
+  
+  tituloDocumento.textContent = nomeDocumento || "Documento sem título";
+  
+  selecionarDocumento(nomeDocumento);
+  
+  textoEditor.addEventListener("keyup", () => {
+    emitirTextoEditor({
+      texto: textoEditor.value,
+      nomeDocumento,
     });
-});
-
-function atualizaTextoEditor(texto) {
-    textoEditor.value = texto; 
-}
-
-export {atualizaTextoEditor};
+  });
+  
+  function atualizaTextoEditor(texto) {
+    textoEditor.value = texto;
+  }
+  
+  export { atualizaTextoEditor };
--- src/servidor.js
+++ src/servidor.js
@@ -2,18 +2,22 @@ import express from "express";
 import url from "url";
 import path from "path";
 import http from "http";
-import {Server} from "socket.io";
+import { Server } from "socket.io";
+
+import "./dbConect.js";
 
 const app = express();
 const porta = process.env.porta || 3000;
 
 const caminhoAtual = url.fileURLToPath(import.meta.url);
-const diretorioPublico  = path.join(caminhoAtual, "../..", "public");
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
 app.use(express.static(diretorioPublico));
 
 const servidorHttp = http.createServer(app);
 
-servidorHttp.listen(porta, () => console.log(`Servidor Escutando na porta ${porta}`))
+servidorHttp.listen(porta, () =>
+  console.log(`Servidor escutando na porta ${porta}`)
+);
 
 const io = new Server(servidorHttp);
 
